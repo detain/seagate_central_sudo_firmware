31109437af7223c29cdc718f81bfea8741543e4f
Joe Huss <detain@interserver.net>
2015-08-25 05:26:34 -0400

    updates to the patch adding in supprot for some erro checking, mamong other things.

--- seagate_patch_root.sh
+++ seagate_patch_root.sh
@@ -1,22 +1,45 @@
 #!/bin/bash
+img=Seagate-HS-update-201506110006F.img;
+if [ ! -e ${img} ]; then
+	wget 'https://apps1.seagate.com/downloads/certificate.html?action=performDownload&key=941311271907' -O ${img}
+fi;
+orig_year="$(echo "$img" | cut -d- -f4 | cut -c1-4)"
+orig_month="$(echo "$img" | cut -d- -f4 | cut -c5-6)"
+orig_day="$(echo "$img" | cut -d- -f4 | cut -c7-8)"
+release="$(echo "$img" | cut -d- -f4 | cut -c9- | cut -d\. -f1)"
 year=$(date +%Y)
 month=$(date +%m)
 day=$(date +%d)
-if [ ! -e Seagate-HS-update-201506110006F.img ]; then
-	wget 'https://apps1.seagate.com/downloads/certificate.html?action=performDownload&key=941311271907' -O Seagate-HS-update-201506110006F.img
-fi;
-mkdir -p seagate;
-cd seagate;
-tar xvzf ../Seagate-HS-update-201506110006F.img;
+newimg="Seagate-HS-update-${year}${month}${day}${release}.img";
+#mkdir -p seagate;
+#cd seagate;
+tar xvzf ${img};
 unsquashfs rfs.squashfs;
-/bin/mv -f rfs.squashfs rfs.squashfs.orig;
-sed s#"^PermitRootLogin without-password"#"PermitRootLogin yes"#g -i squashfs-root/etc/ssh/sshd_config
-sed s#"\"users,nogroup"#"\"users,nogroup,wheel"#g -i squashfs-root/usr/bin/usergroupmgr.sh;
-sed s#"usermod -a -G nogroup"#"usermod -a -G users,wheel,nogroup"#g -i squashfs-root/usr/sbin/ba-upgrade-finish;
-echo '%wheel ALL=(ALL)       NOPASSWD: ALL' >> squashfs-root/etc/sudoers;
-echo '%users ALL=(ALL)       NOPASSWD: ALL' >> squashfs-root/etc/sudoers;
-#patch -p1 < seagate_patch_root.patch;
+if [ "$(grep "^PermitRootLogin yes" squashfs-root/etc/ssh/sshd_config)" = "" ]; then
+	sed s#"^PermitRootLogin without-password"#"PermitRootLogin yes"#g -i squashfs-root/etc/ssh/sshd_config
+fi;
+if [ "$(grep "\"users,nogroup,wheel" squashfs-root/usr/sbin/ba-upgrade-finish)" = "" ]; then
+	sed s#"\"users,nogroup"#"\"users,nogroup,wheel"#g -i squashfs-root/usr/bin/usergroupmgr.sh;
+fi;
+if [ "$(grep "usermod -a -G users,wheel" squashfs-root/usr/sbin/ba-upgrade-finish)" = "" ]; then
+	sed s#"usermod -a -G nogroup"#"usermod -a -G users,wheel,nogroup"#g -i squashfs-root/usr/sbin/ba-upgrade-finish;
+fi;
+if [ "$(grep "^%wheel.*NOPASS" squashfs-root/etc/sudoers)" = "" ]; then
+	echo '%wheel ALL=(ALL)       NOPASSWD: ALL' >> squashfs-root/etc/sudoers;
+fi;
+if [ "$(grep "^%users.*NOPASS" squashfs-root/etc/sudoers)" = "" ]; then
+	echo '%users ALL=(ALL)       NOPASSWD: ALL' >> squashfs-root/etc/sudoers;
+fi;
+if [ "$(grep "^passwd -d root" squashfs-root/usr/sbin/ba-upgrade-finish)" = "" ]; then
+	echo 'passwd -d root' >> squashfs-root/usr/sbin/ba-upgrade-finish;
+fi;
+/bin/rm -f rfs.squashfs;
 mksquashfs squashfs-root rfs.squashfs;
 md5="$(md5sum rfs.squashfs  | cut -d" " -f1)"
-sed -e s#"=11-06-2015"#"=${day}-${month}-${year}"#g -e s#"=2015\.0611"#"=${year}.${month}${day}"#g -e s#"^rfs=.*$"#"rfs=$md5"#g -i config.ser
-tar cfzv ../Seagate-HS-update-${year}${month}${day}0006F.img rfs.squashfs uImage config.ser
+sed \
+	-e s#"=${orig_day}-${orig_month}-${orig_year}"#"=${day}-${month}-${year}"#g \
+	-e s#"=${orig_year}\.${orig_month}${orig_day}"#"=${year}.${month}${day}"#g \
+	-e s#"^rfs=.*$"#"rfs=$md5"#g \
+	-i config.ser;
+tar cfzv ${newimg} rfs.squashfs uImage config.ser
+echo "Created ${newimg} from ${img}"
